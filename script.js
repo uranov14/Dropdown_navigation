2b1bfa69b3ff857bbecd94330ab80d36e689a66d
uranov14 <102876402+uranov14@users.noreply.github.com>
2022-06-29 22:30:57 +0300

    Сделал віпадающее меню

--- script.js
+++ script.js
@@ -3,13 +3,22 @@ toggle between hiding and showing the dropdown content */
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
+function myFunction2() {
+    document.getElementById("myDropdown2").classList.toggle("show");
+}
   
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(e) {
-if (!e.target.matches('.dropbtn')) {
-var myDropdown = document.getElementById("myDropdown");
-    if (myDropdown.classList.contains('show')) {
-    myDropdown.classList.remove('show');
+    if (!e.target.matches('#features')) {
+        let myDropdown = document.getElementById("myDropdown");
+        if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+        }
     }
-}
+    if (!e.target.matches('#company')) {
+        let myDropdown2 = document.getElementById("myDropdown2");
+        if (myDropdown2.classList.contains('show')) {
+        myDropdown2.classList.remove('show');
+        }
+    }
 }
