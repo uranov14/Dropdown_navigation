36c7254400df1470fba33d1b918fae939c920366
uranov14 <102876402+uranov14@users.noreply.github.com>
2022-07-01 20:40:15 +0300

    Полностью обновил проект

--- script.js
+++ script.js
@@ -1,24 +1,12 @@
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
-function myFunction2() {
-    document.getElementById("myDropdown2").classList.toggle("show");
-}
-  
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(e) {
-    if (!e.target.matches('#features')) {
-        let myDropdown = document.getElementById("myDropdown");
-        if (myDropdown.classList.contains('show')) {
-        myDropdown.classList.remove('show');
-        }
-    }
-    if (!e.target.matches('#company')) {
-        let myDropdown2 = document.getElementById("myDropdown2");
-        if (myDropdown2.classList.contains('show')) {
-        myDropdown2.classList.remove('show');
-        }
-    }
-}
+const menu = document.querySelector('.menu');
+const dropdown = document.querySelectorAll('.dropdown');
+
+menu.addEventListener('click',function(){
+    menu.parentElement.classList.toggle('open');
+    document.body.classList.toggle('nav-open');
+})
+dropdown.forEach(function(item){
+    item.addEventListener('click',function(){
+        item.parentElement.classList.toggle('link-open');
+    })
+})
